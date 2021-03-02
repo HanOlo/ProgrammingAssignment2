e7f9c5a6aae3ae22cb3b175ca34296f0bb9440ac
HanOlo <ingrupen@hotmail.com>
2021-03-01 21:41:45 -0500

    Revert "Completed assignment"
    This reverts commit a54f58e895139a18939c44a5716406ec409ce92b.

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,15 @@
-## makeCacheMatrix is a function with several functions inside.  The first function establishes 
-## additional functions cacheSolve to call from. 
-## i is the global variable that will hold the matrix.  
- 
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function pulls in a matrix and provides functions for cacheSolve to store into memory
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inv) i <<- inv
-  getinverse <- function() i
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
 
-## cacheSolve checks for a cached matrix, if one doesn't exist it creates the inverse and stores it.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
-  if(!is.null(i)) {
-    message("getting cached data")
-    return(i)
-  }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
 }
-
-
-## I copied the 2 functions provided in the exercise and converted m to i.  
-## Next, I converted means to inverse.  Finally, I added the solve function.
-
-
-
-
